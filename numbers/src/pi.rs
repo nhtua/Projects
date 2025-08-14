f69d59c3f970dc25405f043be66c7fba5c736634
Liam Nguyen <64422533+liam102@users.noreply.github.com>
2025-08-14 00:25:34 -0600

    calculate pi in multi threads

--- numbers/src/pi.rs
+++ numbers/src/pi.rs
@@ -1,3 +1,4 @@
+use rayon::prelude::*;
 use rug::Float;
 use rug::ops::Pow;
 
@@ -16,23 +17,21 @@ fn chudnovsky(number_of_term: u32, precision: u32) -> Float {
 }
 fn sigma(number_of_term: u32, precision: u32) -> Float {
     let round: u32 = number_of_term * 14;
-    let mut k: u32 = 0;
-    let mut total: Float = Float::with_val(precision, 0);
-    while k < round {
-        let sign_val = if k % 2 == 0 { 1.0f64 } else { -1.0f64 };
-        let up = Float::with_val(precision, sign_val)
-            * factorial(6 * k, precision)
-            * Float::with_val(precision, 545140134 * k as i64 + 13591409);
-        let down_fact_3k = factorial(3 * k, precision);
-        let down_fact_k_pow3 = factorial(k, precision).pow(Float::with_val(precision, 3.0));
-        let down_const_pow = Float::with_val(precision, 640320.0f64)
-            .pow(Float::with_val(precision, (3 * k) as f64 + 1.5));
-        let down: Float = down_fact_3k * down_fact_k_pow3 * down_const_pow;
-        let sum_a_term = up / down;
-        total += sum_a_term;
-        k += 1;
-    }
-    total
+    (0..round)
+        .into_par_iter()
+        .map(|k| {
+            let sign_val = if k % 2 == 0 { 1.0f64 } else { -1.0f64 };
+            let up = Float::with_val(precision, sign_val)
+                * factorial(6 * k, precision)
+                * Float::with_val(precision, 545140134 * k as i64 + 13591409);
+            let down_fact_3k = factorial(3 * k, precision);
+            let down_fact_k_pow3 = factorial(k, precision).pow(Float::with_val(precision, 3.0));
+            let down_const_pow = Float::with_val(precision, 640320.0f64)
+                .pow(Float::with_val(precision, (3 * k) as f64 + 1.5));
+            let down: Float = down_fact_3k * down_fact_k_pow3 * down_const_pow;
+            up / down
+        })
+        .reduce(|| Float::with_val(precision, 0), |a, b| a + b)
 }
 fn factorial(n: u32, precision: u32) -> Float {
     (2..=n).fold(Float::with_val(precision, 1), |acc, x| {
