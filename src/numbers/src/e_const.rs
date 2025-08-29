c9ebb119f9a616afc783997767d7aeb228434c49
Liam Nguyen <5275627+nhtua@users.noreply.github.com>
2025-08-29 09:48:31 -0600

    small fix

--- src/numbers/src/e_const.rs
+++ src/numbers/src/e_const.rs
@@ -5,7 +5,7 @@ pub fn sprint(len: u32) -> String {
     e.to_string_radix(10, Some((len + 1) as usize))
 }
 pub fn find(len: u32) -> Float {
-    let precision = (len * 4) as u32; // Each term adds about 1 decimal digit, so 4 times the length for precision
+    let precision = len * 4; // Each term adds about 1 decimal digit, so 4 times the length for precision
     let mut e = Float::with_val(precision, 0.0);
     for i in 0..=len {
         let term = Float::with_val(precision, 1.0) / factorial(i, precision);
@@ -18,6 +18,8 @@ fn factorial(n: u32, precision: u32) -> Float {
     if n == 0 {
         Float::with_val(precision, 1.0)
     } else {
-        (1..=n).fold(Float::with_val(precision, 1.0), |acc, x| acc * Float::with_val(precision, x))
+        (1..=n).fold(Float::with_val(precision, 1.0), |acc, x| {
+            acc * Float::with_val(precision, x)
+        })
     }
 }
